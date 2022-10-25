0787e5f736347d99490df535844ba3ffbbf6456f
lassehj0 <lassehj00@github.com>
2022-10-25 12:51:09 +0200

    Added stuff to door class, might be finished

--- ChargingCabinet/Models/Door.cs
+++ ChargingCabinet/Models/Door.cs
@@ -1,14 +1,47 @@
-﻿namespace ChargingCabinet.Models
+﻿using ChargingCabinet.Interfaces;
+
+namespace ChargingCabinet.Models
 {
 	public class Door : IDoor
 	{
-        private doorState;
-		public event EventHandler<DoorChangedEventArgs> DoorEvent;
+		private doorState _state;
+		public event EventHandler<DoorEventArgs> CurrentDoorEvent;
 
-        onDoorOpen()
+		private void NewDoorState(doorState state)
 		{
-			if (doorState == doorState.closed)
-				newDoorState(doorState.opened);
+			if (_state != state)
+			{
+                CurrentDoorEvent?.Invoke(this, new DoorEventArgs { doorState = state });
+                _state = state;
+			}
 		}
+
+		public void onDoorOpen()
+		{
+			if (_state == doorState.Closed)
+				NewDoorState(doorState.Opened);
+		}
+
+		public void onDoorClose()
+		{
+			if (_state == doorState.Opened)
+				NewDoorState(doorState.Closed);
+		}
+
+		public void lockDoor()
+		{
+			if (_state == doorState.Closed)
+				NewDoorState(doorState.Locked);
+			else
+				throw new ArgumentException("Door is not closed and therefore cannot be locked");
+		}
+
+		public void unlockDoor()
+		{
+            if (_state == doorState.Locked)
+                NewDoorState(doorState.Closed);
+            else
+                throw new ArgumentException("Door is not locked and therefore cannot be unlocked");
+        }
 	}
 }
