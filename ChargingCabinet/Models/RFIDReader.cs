b5f6328a3806f3cbb8d4f65c862a2e50801f5035
lassehj0 <lassehj00@github.com>
2022-10-26 12:04:50 +0200

    Implemented RFIDReader class

---
+++ ChargingCabinet/Models/RFIDReader.cs
@@ -0,0 +1,21 @@
+﻿using ChargingCabinet.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ChargingCabinet.Models
+{
+    internal class RFIDReader : IRFIDReader
+    {
+
+        public event EventHandler<RFIDReaderChangedEventArgs>? RFIDReaderChangedEvent;
+
+        public void setRFIDState(int nRFID)
+        {
+            RFIDReaderChangedEvent?
+                .Invoke(this, new RFIDReaderChangedEventArgs { RFIDReaderValue = nRFID } );
+        }
+    }
+}
