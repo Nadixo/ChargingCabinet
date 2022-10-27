5897f95ec26f2984d78f6cfb5d68c3df8210acd0
Clara <claramouritsen@outlook.com>
2022-10-27 15:18:00 +0200

    Added TestFixture and SetUp

---
+++ ChargingCabinetNUnitTest/DoorTester.cs
@@ -0,0 +1,20 @@
+﻿using ChargingCabinet.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ChargingCabinetNUnitTest
+{
+    [TestFixture]
+    public class DoorTester
+    {
+        Door door;
+        [SetUp]
+        public void SetUp()
+        {
+            door = new Door();
+        }
+    }
+}
