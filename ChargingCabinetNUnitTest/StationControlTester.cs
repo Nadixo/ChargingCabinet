65a0f16644bc6c28774b5e80df3fc7a8a484f7b6
Clara <claramouritsen@outlook.com>
2022-10-28 13:59:12 +0200

    Added tester for station control

---
+++ ChargingCabinetNUnitTest/StationControlTester.cs
@@ -0,0 +1,41 @@
+﻿using ChargingCabinet.Interfaces;
+using ChargingCabinet.Models;
+using NSubstitute;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static NUnit.Framework.Constraints.Tolerance;
+
+namespace ChargingCabinetNUnitTest
+{
+    [TestFixture]
+    internal class StationControlTester
+    {
+        private StationControl stationControl;
+        private IDoor door;
+        private IRFIDReader rfidReader;
+        private IUsbCharger charger;
+        private IDisplay display;
+
+        [SetUp]
+        public void Setup()
+        {
+            charger = Substitute.For<IUsbCharger>();
+            display = Substitute.For<IDisplay>();
+            door = Substitute.For<IDoor>();
+            rfidReader = Substitute.For<IRFIDReader>();
+
+            stationControl = new StationControl(door, rfidReader, charger, display);
+        }
+
+        [TestCase(1)]
+        public void OpenDoorDoesNotLockOnRFIDScan(int id)
+        {
+            rfidReader.RFIDReaderChangedEvent += Raise.EventWith(new RFIDReaderEventArgs { RFIDReaderValue = id });
+
+            door.DidNotReceive().lockDoor();
+        }
+    }
+}
