2116504940d1691a8a8d8953d3b6a1b600baaab5
Clara <claramouritsen@outlook.com>
2022-10-28 12:39:09 +0200

    Created chargecontrol tester and added setup

---
+++ ChargingCabinetNUnitTest/ChargeControlTester.cs
@@ -0,0 +1,25 @@
+﻿using ChargingCabinet.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ChargingCabinetNUnitTest
+{
+    [TestFixture]
+    public class ChargeControlTester
+    {
+        private ChargeControl? charger;
+        private UsbChargerSimulator? chargerSimulator;
+        private Display? display;
+
+        [SetUp]
+        public void Setup()
+        {
+            chargerSimulator = new UsbChargerSimulator();
+            display = new Display();
+            charger = new ChargeControl(chargerSimulator, display);
+        }
+    }
+}
