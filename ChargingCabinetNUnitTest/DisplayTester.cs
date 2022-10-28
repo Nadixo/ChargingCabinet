d76dc86351fd19491cf3d13759bfba037ed4f994
Clara <claramouritsen@outlook.com>
2022-10-28 11:35:48 +0200

    Added display tests

---
+++ ChargingCabinetNUnitTest/DisplayTester.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ChargingCabinetNUnitTest
+{
+    [TestFixture]
+    public class DisplayTester
+    {
+        Display display;
+        [SetUp]
+        public void SetUp()
+        {
+            display = new Display();
+        }
+
+        [TestCase("A normal string")]
+        [TestCase("A gr8 string with numbers i guess")]
+        [TestCase("H¤w @b¤ut $¤m€ $p€ci@l ch@r@ct€rs")]
+        [TestCase("")]
+        public void TestIfOutputIsTheSameAsString(string s)
+        {
+            var stringWriter = new StringWriter();
+            Console.SetOut(stringWriter);
+
+            display.ShowDisplay(s);
+            Assert.That(s, Is.EqualTo(stringWriter.ToString()));
+        }
+    }
+}
