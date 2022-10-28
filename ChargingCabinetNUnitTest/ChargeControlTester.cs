4256eb284189bb82295abc508ee5246cbdfd76fc
Clara <claramouritsen@outlook.com>
2022-10-28 13:20:51 +0200

    Added test to check for state change on event handler call

--- ChargingCabinetNUnitTest/ChargeControlTester.cs
+++ ChargingCabinetNUnitTest/ChargeControlTester.cs
@@ -1,9 +1,13 @@
 ﻿using ChargingCabinet.Interfaces;
+using ChargingCabinet.Models;
+using ChargingCabinet.Simulators;
 using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using static ChargingCabinet.Interfaces.IUsbCharger;
+using static NUnit.Framework.Constraints.Tolerance;
 
 namespace ChargingCabinetNUnitTest
 {
@@ -11,7 +15,7 @@ namespace ChargingCabinetNUnitTest
     public class ChargeControlTester
     {
         private ChargeControl? charger;
-        private UsbChargerSimulator? chargerSimulator;
+        private UsbChargerSimulator chargerSimulator;
         private Display? display;
 
         [SetUp]
@@ -20,6 +24,22 @@ namespace ChargingCabinetNUnitTest
             chargerSimulator = new UsbChargerSimulator();
             display = new Display();
             charger = new ChargeControl(chargerSimulator, display);
+        }
+
+        [TestCase(int.MaxValue, UsbChargerState.stopCharging)]
+        [TestCase(500.1, UsbChargerState.stopCharging)]
+        [TestCase(500, UsbChargerState.charging)]
+        [TestCase(499.9, UsbChargerState.charging)]
+        [TestCase(5.1, UsbChargerState.charging)]
+        [TestCase(5, UsbChargerState.fullyCharged)]
+        [TestCase(4.9, UsbChargerState.fullyCharged)]
+        [TestCase(0.1, UsbChargerState.fullyCharged)]
+        [TestCase(0, UsbChargerState.notCharging)]
+        public void CurrentEventCheckState(double current, UsbChargerState state)
+        {
+            charger?.HandleCurrentEvent(chargerSimulator, new CurrentEventArgs { Current = current});
+
+            Assert.That(state, Is.EqualTo(charger?.State));
         }
     }
 }
